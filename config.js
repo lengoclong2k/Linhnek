066d9593ba7f0d11f4b1a87a3a77fc3f58b942af
Frankess.Sus <ngoclong28112020@gmail.com>
2021-10-20 09:26:29 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hey Linh',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Xem có gì tiếp theo nào <3',
+    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu Long nhất trên đời',
+    btnNo: 'Không, chúng ta không yêu nhau:3',
+    question: 'Cho anh một lí do sao chúng ta yêu nhau <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Yêu em nhiều',
+    btnAccept: 'OKIII <3',
+    messLink: 'https://www.facebook.com/ngoclong2811/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
